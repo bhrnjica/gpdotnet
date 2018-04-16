9acfe13a2c4411b6bea13dc409faea3a7c0eadbf
Bahrudin Hrnjica <bhrnjica@BAHR-NB01>
2018-04-16 18:53:46 +0200

    GpdotNET v5.01 release

--- Net/GPdotNET.Wnd.App/MainWindow.Designer.cs
+++ Net/GPdotNET.Wnd.App/MainWindow.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Start Page");
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Start Page");
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.treeView1 = new System.Windows.Forms.TreeView();
@@ -88,7 +88,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer1.Location = new System.Drawing.Point(0, 102);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(5);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -114,12 +114,12 @@
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Margin = new System.Windows.Forms.Padding(0);
             this.treeView1.Name = "treeView1";
-            treeNode2.ImageKey = "start.png";
-            treeNode2.Name = "Node0";
-            treeNode2.Tag = "startpage";
-            treeNode2.Text = "Start Page";
+            treeNode1.ImageKey = "start.png";
+            treeNode1.Name = "Node0";
+            treeNode1.Tag = "startpage";
+            treeNode1.Text = "Start Page";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode2});
+            treeNode1});
             this.treeView1.SelectedImageIndex = 0;
             this.treeView1.Size = new System.Drawing.Size(170, 395);
             this.treeView1.TabIndex = 0;
@@ -157,7 +157,7 @@
             this.tabPage3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage3.Size = new System.Drawing.Size(754, 369);
             this.tabPage3.TabIndex = 0;
             this.tabPage3.Text = "Functions";
@@ -168,7 +168,7 @@
             this.tabPage4.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage4.Size = new System.Drawing.Size(754, 369);
             this.tabPage4.TabIndex = 1;
             this.tabPage4.Text = "Parameters";
@@ -179,7 +179,7 @@
             this.tabPage5.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage5.Size = new System.Drawing.Size(754, 369);
             this.tabPage5.TabIndex = 2;
             this.tabPage5.Text = "Run";
@@ -190,7 +190,7 @@
             this.tabPage6.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage6.Location = new System.Drawing.Point(4, 22);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage6.Size = new System.Drawing.Size(754, 369);
             this.tabPage6.TabIndex = 3;
             this.tabPage6.Text = "Solution";
@@ -201,7 +201,7 @@
             this.tabPage7.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage7.Location = new System.Drawing.Point(4, 22);
             this.tabPage7.Name = "tabPage7";
-            this.tabPage7.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage7.Size = new System.Drawing.Size(754, 369);
             this.tabPage7.TabIndex = 4;
             this.tabPage7.Text = "Validation";
@@ -224,7 +224,7 @@
             this.tabPage1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(754, 369);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Data";
@@ -235,7 +235,7 @@
             this.tabPage2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(754, 369);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Info";
@@ -289,7 +289,7 @@
             this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.ribbon1.ForeColor = System.Drawing.Color.DarkViolet;
             this.ribbon1.Location = new System.Drawing.Point(0, 0);
-            this.ribbon1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ribbon1.Margin = new System.Windows.Forms.Padding(2);
             this.ribbon1.Minimized = false;
             this.ribbon1.Name = "ribbon1";
             // 
@@ -322,7 +322,7 @@
             this.ribbonTab1.Panels.Add(this.ribbonPanel2);
             this.ribbonTab1.Panels.Add(this.ribbonPanel3);
             this.ribbonTab1.Panels.Add(this.ribbonPanel4);
-            this.ribbonTab1.Text = "GPdotNET";
+            this.ribbonTab1.Text = "GPdotNET v5.01";
             // 
             // ribbonPanel1
             // 
@@ -607,7 +607,7 @@
             this.Controls.Add(this.ribbon1);
             this.ForeColor = System.Drawing.Color.DarkViolet;
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(898, 554);
             this.Name = "MainWindow";
             this.splitContainer1.Panel1.ResumeLayout(false);
